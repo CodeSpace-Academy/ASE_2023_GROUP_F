df6fd6be46172c8694382a459a708ba354b21cce
Tumii01 <tumisangnke@gmail.com>
2023-10-09 16:33:20 +0200

    Creation of Helpers Folder & Connecting to the Database

---
+++ helpers/db.js
@@ -0,0 +1,13 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export default client;
