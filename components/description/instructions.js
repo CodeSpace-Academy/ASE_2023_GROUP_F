c4a960e8d0749a19882c6bc6322359f41c9189af
Akeem Brown <117109568+Akido123@users.noreply.github.com>
2023-10-10 10:06:45 +0200

    Fragment removed instructions.js
    Replaced Fragment with <>

--- components/description/instructions.js
+++ components/description/instructions.js
@@ -1,17 +1,15 @@
-import { Fragment } from "react";
-
 function Instructions(props){
   const { instructions } = props
 
   return(
-    <Fragment>
+    <>
       {instructions.map((instruction, index) => {
         <div>
           <h4>{index}</h4>
           <p>{instruction}</p>
         </div>
       })}
-    </Fragment>
+    </>
   )
 }
 
