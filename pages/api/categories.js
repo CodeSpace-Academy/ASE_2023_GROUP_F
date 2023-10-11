41c503c832ed4621a74423cb90c1b505ce83cfe6
Tumii01 <tumisangnke@gmail.com>
2023-10-11 10:42:10 +0200

    Addition of Categories Api route

---
+++ pages/api/categories.js
@@ -0,0 +1,21 @@
+import client from "../../helpers/db";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      await client.connect();
+      const database = client.db("devdb");
+      const collection = database.collection("categories");
+      const documents = await collection.find({}).toArray();
+
+      res.status(200).json({ recipes: documents });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ message: "Data fetching failed" });
+    } finally {
+      await client.close();
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
