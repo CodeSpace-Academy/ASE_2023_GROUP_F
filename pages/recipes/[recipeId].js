c77be8ce23d240e49cd3f11dd61182d0c641f4d2
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-10-13 14:18:31 +0200

    added page routes

--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -3,10 +3,9 @@ import React from 'react'
 function SingleRecipe() {
   return (
     <div>
-      <h1>Single Recipe</h1>
+      <h1>Single Recipes</h1>
     </div>
   )
 }
 
 export default SingleRecipe
-<h1>
