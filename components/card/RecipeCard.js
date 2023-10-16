b9284513ea571e85ce7ce1cbabadeba3d8ebc77e
NelsonMALGAS <zmalgas69@gmail.com>
2023-10-16 13:49:02 +0200

    Modified card

--- components/card/RecipeCard.js
+++ components/card/RecipeCard.js
@@ -19,13 +19,11 @@ const RecipeCard = (props) => {
 
 	return (
 		<div className="max-w-xm rounded overflow-hidden shadow-lg mx-auto my-4 relative transition-transform duration-300 transform hover:scale-105">
-			<div className="h-30 relative">
+			<div className="h-80 relative">
 				<img
 					className="w-full h-full object-cover"
 					src={images[currentImage]}
 					alt={`Recipe image ${currentImage + 1}`}
-					width="250px"
-					height="350px"
 				/>
 				<button
 					className="absolute top-1/2 transform -translate-y-1/2 left-4 text-6xl text-blue hover:text-indigo-500 transition duration-300 ease-in-out"
@@ -41,13 +39,15 @@ const RecipeCard = (props) => {
 				</button>
 			</div>
 			<div className="px-6 py-4 bg-gray-200 rounded">
-				<div className="font-bold text-xl mb-2">{title}</div>
-				<div className="flex items-center gap-2">
-					<DateIcon />
-					<time>{humanReadable}</time>
-				</div>
-				<div className="flex items-center gap-2 mx-20 transition-transform transform hover:scale-150">
-					<ArrowIcon />
+				<div className="flex flex-col h-40 justify-between">
+					<div className="font-bold text-xl">{title}</div>
+					<div className="flex items-center gap-2">
+						<DateIcon />
+						<time>{humanReadable}</time>
+					</div>
+					<div className="flex items-center gap-2 mx-20 transition-transform transform hover:scale-150">
+						<ArrowIcon />
+					</div>
 				</div>
 			</div>
 		</div>
