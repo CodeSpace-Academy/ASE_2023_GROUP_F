bf0eebb53817bd8999b6cdfb47b528ae7fbecf8d
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-10-16 22:10:26 +0200

    correctly fetched db data inplace of dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-export default function Home() {
-  return (
-    <>
-      <h1>HomePage</h1>
-    </>
-  )
-}
+import {getViewRecipes} from "../lib/view-recipes"
+import { useEffect } from "react";
+
+
+const Home = () => {
+	useEffect(() => {
+		const callGetRecipes = async () => {
+			const results = await getViewRecipes()
+			console.log('results = ', results)
+		}
+		callGetRecipes()
+	},[])
+	return(
+		<h1>Home page</h1>
+	)
+};
+
+export default Home;
--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -1,34 +1,48 @@
-import { getRecipeById } from "@/dummy_data";
 import Details from "@/components/details/details";
+import { getViewRecipes } from "@/lib/view-recipes";
+import { useEffect, useState } from "react";
 
-function SingleRecipe({ recipe, error}) {
+function getRecipeById(array, id) {
+  return array.find((recipe) => recipe._id === id);
+}
+
+function SingleRecipe({ recipeId }) {
+  const [recipes, setRecipes] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      try{
+        const results = await getViewRecipes();
+        setRecipes(results)
+      }catch(error){
+        console.log(`something went wrong: ${error}`);
+        setError(error)
+      }
+    }
+
+    getRecipes()
+  }, [])
+
+  const arrayOfRecipes = Object.entries(recipes)
+
+  const actualRecipe = getRecipeById(arrayOfRecipes[0][1], recipeId);
+
   return (
     <>
-      <Details recipe={recipe} error={error}/>
+      <Details recipe={actualRecipe} error={error}/>
     </>
   );
 }
 
-export async function getServerSideProps({ params }) {
-  const { recipeId } = params;
-  let recipe = null;
-  let error = false;
-
-  try {
-    // Fetch the recipe data from the API based on recipeId
-    recipe = await getRecipeById(recipeId);
-  } catch (e) {
-    // Handle the error and set the 'error' flag
-    console.error("Error fetching recipe:", e);
-    error = true;
-  }
-
-  return {
-    props: {
-      recipe,
-      error,
-    },
-  };
-}
-
 export default SingleRecipe;
+
+export async function getServerSideProps({params}){
+  const {recipeId} = params
+
+  return{
+    props: {
+      recipeId
+    }
+  }
+}
