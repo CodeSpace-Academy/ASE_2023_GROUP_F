08b9760b2e98b7c7e6c680beebc51cb5d06b905f
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-10-17 13:25:24 +0200

    Modified the time component

---
+++ components/icons/TimeIcon.js
@@ -0,0 +1,7 @@
+const TimeIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+    <path d="M12 1C6.48 1 2 6.48 2 12s4.48 11 10 11 10-4.48 10-10S17.52 1 12 1zm0 20c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-10H11V6h2v5z"/>
+  </svg>
+);
+
+export default TimeIcon;
