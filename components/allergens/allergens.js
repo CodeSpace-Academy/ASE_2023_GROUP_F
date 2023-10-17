49e0fde7cb5e1dc66822f77afbda7aa3374ecd0b
Tumii01 <tumisangnke@gmail.com>
2023-10-17 14:00:05 +0200

    Addition of the code that Checks for allergens

--- components/allergens/allergens.js
+++ components/allergens/allergens.js
@@ -1,15 +1,41 @@
 import React from 'react';
 
 function RecipeAllergens(props) {
-  const { allergens } = props;
+  const { allergens, ingredients } = props;
+
+  const allergensInIngredients = () => {
+    const allergensFound = [];
+
+    for (const allergen of allergens) {
+      for (const ingredientKey in ingredients) {
+        if (ingredients.hasOwnProperty(ingredientKey)) {
+          const ingredient = ingredients[ingredientKey];
+
+          if (ingredient.toLowerCase().includes(allergen.toLowerCase())) {
+            allergensFound.push(allergen);
+            break;
+          }
+        }
+      }
+    }
+
+    return allergensFound;
+  };
+
+  const allergensFound = allergensInIngredients();
 
   return (
     <div>
-      <ul>
-        {allergens.map((allergen) => (
-          <li key={allergen}>{allergen}</li>
-        ))}
-      </ul>
+      <h3>Allergens Found:</h3>
+      {allergensFound.length > 0 ? (
+        <ul>
+          {allergensFound.map((allergen) => (
+            <li key={allergen}>{allergen}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No allergens found in the ingredients.</p>
+      )}
     </div>
   );
 }
