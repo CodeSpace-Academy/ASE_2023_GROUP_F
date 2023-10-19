7a6a75810788fc00ba8ad2947d3648f6c8d92835
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-10-19 14:16:21 +0200

    added tags

--- components/tags/RecipeTags.js
+++ components/tags/RecipeTags.js
@@ -1,15 +1,26 @@
-import React from 'react'; 
+import React from 'react';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
 
-    function RecipeTags(props) {
-      const { tags } = props; 
+function RecipeTags(props) {
+  const { tags } = props;
+
+  if (!tags) {
+    return <div>No tags available</div>;
+  }
+
+  
+
+  return (
+  
+    <ul className="list-none p-0">
+      {tags.map((tag) => (
+        <li key={tag} className="inline-block m-2 bg-gray-200 rounded-md px-4 py-2">
+          {tag}
+        </li>
+      ))}
+    </ul>
     
-      return (
-        <div>
-          {tags.map(tag => (
-            <button key={tag}>{tag}</button> 
-          ))}
-        </div>
-      );
-    }
-    
-    export default RecipeTags;
+  );
+}
+
+export default RecipeTags
