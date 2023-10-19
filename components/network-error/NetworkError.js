92e7729a5702852e936e52109b7d46413c6a7821
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-10-19 10:38:04 +0200

    Styling error component

--- components/network-error/NetworkError.js
+++ components/network-error/NetworkError.js
@@ -4,7 +4,8 @@ const HandleNetworkError = ({ children, errorType }) => {
 
   if (errorMessage) {
     return (
-      <div>
+      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative" role="alert">
+        <p className="font-bold">Error:</p>
         <p>{errorMessage}</p>
       </div>
     );
@@ -12,6 +13,7 @@ const HandleNetworkError = ({ children, errorType }) => {
 
   return children;
 };
+
 
 //The getErrorMessage function maps the errorType to the appropriate error message.
 function getErrorMessage(errorType) {
