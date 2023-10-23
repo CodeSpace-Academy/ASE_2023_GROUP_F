b670ff8153072336506d005b2a1b89c449c37009
NelsonMALGAS <zmalgas69@gmail.com>
2023-10-23 11:57:36 +0200

    Updated the show more btn

--- components/recipe-collection/RecipeList.js
+++ components/recipe-collection/RecipeList.js
@@ -5,96 +5,58 @@ import { getViewRecipes } from '../../lib/view-recipes';
 import Button from '../UI/Button';
 
 const PAGE_SIZE = 50;
+const INITIAL_LOAD_SIZE = 50;
 
-const RecipeList = ({ recipes, count }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [visibleRecipes, setVisibleRecipes] = useState(recipes.slice(0, PAGE_SIZE));
+const RecipeList = (props) => {
+  const { recipes, count } = props;
+  const [visibleRecipes, setVisibleRecipes] = useState(recipes);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const totalPages = Math.ceil(count / PAGE_SIZE);
-  const remainingRecipes = count - currentPage * PAGE_SIZE;
+  const remainingRecipes = count - visibleRecipes.length;
 
-  const showMoreRecipes = async () => {
-    if (currentPage < totalPages) {
-      setLoading(true);
-      try {
-        const nextPage = currentPage + 1;
-        const result = await getViewRecipes(nextPage * PAGE_SIZE);
-        setVisibleRecipes(result.recipes); 
-        setCurrentPage(nextPage);
-      } catch (error) {
-        console.error('Error fetching more recipes:', error);
-      } finally {
-        setLoading(false);
-      }
+
+  const loadMoreRecipes = async () => {
+    setLoading(true);
+    try {
+      const startIndex = currentPage * PAGE_SIZE ; 
+      const result = await getViewRecipes(startIndex, PAGE_SIZE);
+      setVisibleRecipes([...visibleRecipes, ...result.recipes]);
+      setCurrentPage(currentPage + 1);
+    } catch (error) {
+      console.error('Error fetching more recipes:', error);
+    } finally {
+      setLoading(false);
     }
   };
-
-  const showPreviousRecipes = async () => {
-    if (currentPage > 1) {
-      setLoading(true);
-      try {
-        const previousPage = currentPage - 1;
-        const result = await getViewRecipes((previousPage - 1) * PAGE_SIZE);
-        setVisibleRecipes(result.recipes);
-        setCurrentPage(previousPage);
-      } catch (error) {
-        console.error('Error fetching previous recipes:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    setLoading(false);
-  }, [visibleRecipes]);
+  
 
   return (
-    <>
-      {!loading && visibleRecipes.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {visibleRecipes.map((recipe) => (
-            <RecipeCard
-              key={recipe._id}
-              title={recipe.title}
-              images={recipe.images}
-              published={recipe.published}
-              recipe={recipe}
-            />
-          ))}
-        </div>
-      ) : (
-        <CardSkeleton />
-      )}
-
-      {!loading && totalPages > 1 && (
-        <div className="flex justify-between mt-4">
-          {currentPage > 1 && (
-            <button
-              onClick={showPreviousRecipes}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Previous
-            </button>
-          )}
-          {currentPage < totalPages && (
-            <button
-              onClick={showMoreRecipes}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Next
-            </button>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      {visibleRecipes.map((recipe) => (
+        <RecipeCard
+          key={recipe._id}
+          title={recipe.title}
+          images={recipe.images}
+          published={recipe.published}
+          recipe={recipe}
+        />
+      ))}
+      {loading && <div className="skeleton-container"><CardSkeleton/></div>} 
+      {count > INITIAL_LOAD_SIZE && (
+        <div className="mt-4 text-center">
+          <p className="text-gray-500">
+            Showing page {currentPage} of {totalPages}
+          </p>
+          {remainingRecipes > 0 && (
+            <div className="mt-2">
+              <Button remainingRecipes={remainingRecipes} onClick={loadMoreRecipes} />
+            </div>
           )}
         </div>
       )}
-
-      {remainingRecipes > 0 && (
-        <div className="mt-4">
-          <Button remainingRecipes={remainingRecipes} />
-        </div>
-      )}
-    </>
+    </div>
   );
 };
 
