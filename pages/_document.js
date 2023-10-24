1094c4722d8195c1ff6b1734e9f2cf6280d4ce16
chemonique <122254404+chemonique@users.noreply.github.com>
2023-10-24 12:50:29 +0200

    viewport test

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       <Head />
       <body>
         <Main />
         <NextScript />
-        <script src = "index.js"></script>
+        <script src="index.js"></script>
       </body>
     </Html>
-  )
+  );
 }
