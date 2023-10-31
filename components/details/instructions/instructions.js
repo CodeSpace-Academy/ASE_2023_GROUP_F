ea4ceb49f1f22eb0fbd487a8176a7dad30f63363
chemonique <122254404+chemonique@users.noreply.github.com>
2023-10-31 06:38:07 +0200

    changed layout

--- components/details/instructions/instructions.js
+++ components/details/instructions/instructions.js
@@ -1,66 +1,68 @@
-import React, { useState, useEffect } from "react";
-import { Card, Button, TextField } from "@mui/material";
+import React, { useState, useEffect } from 'react'
+import { Card, Button, TextField } from '@mui/material'
 
 function Instructions(props) {
-  const { recipeId, instructions, userName } = props;
-  const [editableIndex, setEditableIndex] = useState(-1);
-  const [editedInstructions, setEditedInstructions] = useState([...instructions]);
-  const [modifiedInstructions, setModifiedInstructions] = useState({});
+  const { recipeId, instructions, userName } = props
+  const [editableIndex, setEditableIndex] = useState(-1)
+  const [editedInstructions, setEditedInstructions] = useState([
+    ...instructions,
+  ])
+  const [modifiedInstructions, setModifiedInstructions] = useState({})
 
   const handleEdit = (index) => {
-    setEditableIndex(index);
-  };
+    setEditableIndex(index)
+  }
 
   const handleSave = async () => {
     try {
-      const currentDate = new Date();
+      const currentDate = new Date()
       const options = {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      };
-      const formattedDate = currentDate.toLocaleDateString(undefined, options);
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      }
+      const formattedDate = currentDate.toLocaleDateString(undefined, options)
 
       const updatedInstructions = editedInstructions.map(
         (instruction, index) => {
           if (modifiedInstructions[index]) {
-            return `${instruction} (edited by ${userName} on ${formattedDate})`;
+            return `${instruction} (edited by ${userName} on ${formattedDate})`
           }
-          return instruction;
+          return instruction
         }
-      );
+      )
 
       const response = await fetch(`/api/updateRecipe/${recipeId}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ instructions: updatedInstructions }),
-      });
+      })
 
       if (response.ok) {
-        console.log("Recipe updated successfully");
+        console.log('Recipe updated successfully')
       } else {
-        console.error("Failed to update the recipe");
+        console.error('Failed to update the recipe')
       }
     } catch (error) {
-      console.error("Error updating recipe:", error);
+      console.error('Error updating recipe:', error)
     }
-  };
+  }
 
   const handleInputChange = (index, value) => {
-    const modifiedInstructionsCopy = { ...modifiedInstructions };
-    modifiedInstructionsCopy[index] = true;
-    setModifiedInstructions(modifiedInstructionsCopy);
+    const modifiedInstructionsCopy = { ...modifiedInstructions }
+    modifiedInstructionsCopy[index] = true
+    setModifiedInstructions(modifiedInstructionsCopy)
 
-    const updatedInstructions = [...editedInstructions];
-    updatedInstructions[index] = value;
-    setEditedInstructions(updatedInstructions);
-  };
+    const updatedInstructions = [...editedInstructions]
+    updatedInstructions[index] = value
+    setEditedInstructions(updatedInstructions)
+  }
 
   return (
     <div>
-      <div className="bg-green-500 h-96 overflow-y-auto">
+      <div className=" overflow-y-auto">
         {instructions.map((item, index) => (
           <Card key={index} className="m-8 p-8">
             {editableIndex === index ? (
@@ -90,7 +92,7 @@ function Instructions(props) {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default Instructions;
+export default Instructions
