c5f3cede6703adac9e7f70df99866177a4c5bb02
NelsonMALGAS <zmalgas69@gmail.com>
2023-10-31 16:13:08 +0200

    Fixed favorites logic

---
+++ pages/api/favorites.js
@@ -0,0 +1,19 @@
+
+import connectToDatabase from "../../database/database";
+
+export default async function handler(req, res) {
+	if (req.method === "GET") {
+		try {
+			const database = await connectToDatabase();
+			const collection = database.collection("recipes");
+			const documents = await collection.find({ isFavorite: true }).toArray();
+
+			res.status(200).json({ favoriteRecipes: documents });
+		} catch (error) {
+			console.error("Error fetching favorite recipes:", error);
+			res.status(500).json({ message: "Favorite recipes fetching failed" });
+		}
+	} else {
+		res.status(405).json({ message: "Method not allowed" });
+	}
+}
