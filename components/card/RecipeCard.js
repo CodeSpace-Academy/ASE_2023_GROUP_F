6bfb5b7977e01cdb8308351e04d9c330cd1daaee
Tumii01 <tumisangnke@gmail.com>
2023-11-06 14:07:08 +0200

    Edited layout of the cards

--- components/card/RecipeCard.js
+++ components/card/RecipeCard.js
@@ -68,77 +68,68 @@ const RecipeCard = (props) => {
   const isButtonVisible = images.length > 1;
 
   return (
-  <div className="bg-slate-200 rounded-lg shadow-lg max-w-sm transition duration-300 ease-in-out hover:scale-105 ">
-    <div className="relative mx-8 mt-6 rounded-lg">
-      <div className=" h-56 overflow-hidden">
-        <Image
-          className="rounded-lg"
-          width={300}
-          height={300}
-          src={images[currentImage]}
-          alt={`Recipe image ${currentImage + 1}`}
-          layout="fixed"
-        />
-      </div>
-      {isButtonVisible && (
-        <div className="absolute inset-0 flex items-center justify-between p-4">
-          <button
-            className="p-1 rounded-full shadow bg-white text-gray-800 hover:bg-white"
-            onClick={() => changeImage(-1)}
-          >
-            <ChevronLeft size={20} />
-          </button>
-          <button
-            className="p-1 rounded-full shadow bg-white text-gray-800 hover:bg-white"
-            onClickCapture={() => changeImage(1)}
-          >
-            <ChevronRight size={20} />
-          </button>
+    <div className="bg-slate-200  rounded-lg shadow-lg max-w-sm transition duration-300 ease-in-out hover:scale-105 ">
+      <div className="relative rounded-lg">
+        <div className=" h-56 overflow-hidden">
+          <Image
+            className="rounded-lg"
+            width={400}
+            height={300}
+            src={images[currentImage]}
+            alt={`Recipe image ${currentImage + 1}`}
+            layout="fixed"
+          />
         </div>
-      )}
+        {isButtonVisible && (
+          <div className="absolute inset-0 flex items-center justify-between p-4">
+            <button
+              className="p-1 rounded-full shadow bg-white text-gray-800 hover:bg-white"
+              onClick={() => changeImage(-1)}
+            >
+              <ChevronLeft size={20} />
+            </button>
+            <button
+              className="p-1 rounded-full shadow bg-white text-gray-800 hover:bg-white"
+              onClickCapture={() => changeImage(1)}
+            >
+              <ChevronRight size={20} />
+            </button>
+          </div>
+        )}
 
-      <div className="bg-secondary-100 text-red-500 text-xs uppercase font-bold rounded-full p-2 absolute top-0 right-0  mr-2 mt-2">
-        <div className="flex items-center justify-center gap-2">
-         
-          <div style={{ cursor: "pointer" }} onClick={toggleFavorite}>
-            {isFavorite ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
+        <div className="bg-secondary-100 text-red-500 text-xs uppercase font-bold rounded-full p-2 absolute top-0 right-0  mr-2 mt-2">
+          <div className="flex items-center justify-center gap-2">
+            <div style={{ cursor: "pointer" }} onClick={toggleFavorite}>
+              {isFavorite ? (
+                <FavoriteIcon color="error" />
+              ) : (
+                <FavoriteBorderIcon />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="inset-0 transform">
-      <div className="px-6 py-4 rounded-lg mx-8 hover:text-black hover:bg-slate-300">
-        <div className="flex flex-col justify-between">
-          <div className="font-semibold font-serif text-lg flex justify-center items-center p-4  shadow-sm mb-7 rounded-xl">
+      <div className="inset-0 transform">
+        <div className="px-6   hover:text-black hover:bg-slate-300">
+          <div className="flex flex-col justify-between">
+            <div className="font-bold font-serif text-lg pt-2 mb-4 ">
             <strong>{title}</strong>
-          </div>
-          <div className="flex items-center gap-2 p-2">
-            <span className="meaningful-class-name"></span>
-            <TimeDisplay recipe={recipe} />
-          </div>
-          <div className="flex items-center ml-4 gap-2 p-1">
-            {recipe.servings && (
-              <>
-                <ServingIcon width="25" height="25" fill="#2B5B95" />
-                <p>
-                  <strong>Serving:</strong> {recipe.servings}
-                </p>
-              </>
-            )}
+            </div>
+            <div>
+              <TimeDisplay recipe={recipe} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="flex justify-center items-center py-3">
-      <button className=" hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-blue-950 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
-        <Link href={`/recipes/${recipe._id}`}>View Recipe</Link>
-      </button>
+      <div className="flex justify-center items-center py-3">
+        <button className=" hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5  text-center mr-2 mt-2 dark:border-gray-600 dark:text-blue-950 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+          <Link href={`/recipes/${recipe._id}`}>View Recipe</Link>
+        </button>
+      </div>
     </div>
-  </div>
-);
-
+  );
 };
 
 export default RecipeCard;
