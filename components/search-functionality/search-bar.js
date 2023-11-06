2a6a2e8750e68034e85287918bf9992acdc31f2e
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-11-06 13:47:35 +0200

    fixed debounce

--- components/search-functionality/search-bar.js
+++ components/search-functionality/search-bar.js
@@ -81,138 +81,135 @@ const SearchBar = ({
   };
 
   useEffect(() => {
-    if (searchTerm.length < 10) {
-      const debouncedApplyFilters = debounce((title) => {
-        applyFilters({ title });
-      }, 500);
-      debouncedApplyFilters(searchTerm);
+    let timeoutId;
 
-      return () => {
-        debouncedApplyFilters.cancel();
-      };
+    if (searchTerm.length < 10) {
+      timeoutId = setTimeout(() => {
+        applyFilters({ title: searchTerm });
+      }, 500);
     } else {
-      
-      applyFilters({ title: searchTerm });
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
     }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
   }, [searchTerm]);
 
-	return (
-		<div>
-			<div className="flex container items-center justify-between">
-				<Button
-					variant="outlined"
-					size="large"
-					onClick={handleOpen}
-					className="border-gray-800 dark:text-blue-950 hover:text-white border hover:bg-gray-900 rounded-full"
-				>
-					Filters
-				</Button>
-				<div className="flex mx-auto gap-80 items-center space-x-5">
-					<label htmlFor="search" />
-					<input
-						className="rounded text-2xl p-2"
-						type="text"
-						id="search"
-						placeholder="Search...."
-						value={searchTerm}
-						onChange={(e) => setSearchTerm(e.target.value)}
-					/>
+  return (
+    <div>
+      <div className="flex container items-center justify-between">
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={handleOpen}
+          className="border-gray-800 dark:text-blue-950 hover:text-white border hover:bg-gray-900 rounded-full"
+        >
+          Filters
+        </Button>
+        <div className="flex mx-auto gap-80 items-center space-x-5">
+          <label htmlFor="search" />
+          <input
+            className="rounded text-2xl p-2"
+            type="text"
+            id="search"
+            placeholder="Search...."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
 
-					<FormControl
-						className="border-gray-800 hover:bg-slate-200"
-						sx={{ m: 1, minWidth: 120 }}
-					>
-						<InputLabel htmlFor="grouped-native-select">Sort By</InputLabel>
-						<Select
-							native
-							defaultValue=""
-							id="grouped-native-select"
-							label="Grouping"
-							name="sortOption"
-							value={sortOption}
-							onChange={handleSort}
+          <FormControl
+            className="border-gray-800 hover:bg-slate-200"
+            sx={{ m: 1, minWidth: 120 }}
+          >
+            <InputLabel htmlFor="grouped-native-select">Sort By</InputLabel>
+            <Select
+              native
+              defaultValue=""
+              id="grouped-native-select"
+              label="Grouping"
+              name="sortOption"
+              value={sortOption}
+              onChange={handleSort}
+            >
+              <option aria-label="None" value="" />
+              <optgroup name="prep" label="Prep Time">
+                <option value="prep ASC">Prep ASC</option>
+                <option value="prep DESC">Prep DESC</option>
+              </optgroup>
+              <optgroup name="cook" label="Cook Time">
+                <option value="cook ASC">Cook ASC</option>
+                <option value="cook DESC">Cook DESC</option>
+              </optgroup>
+              <optgroup name="published" label="Date Created">
+                <option value="date ASC">Date ASC</option>
+                <option value="date DESC">Date DESC</option>
+              </optgroup>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
 
-						>
-							<option aria-label="None" value="" />
-							<optgroup
-							name= 'prep' 
-							label="Prep Time">
-								<option value='prep ASC'>Prep ASC</option>
-								<option value='prep DESC'>Prep DESC</option>
-							</optgroup>
-							<optgroup
-							name= 'cook'
-							 label="Cook Time">
-								<option value='cook ASC'>Cook ASC</option>
-								<option value='cook DESC'>Cook DESC</option>
-							</optgroup>
-							<optgroup 
-							name= 'published'
-							label="Date Created">
-								<option value='date ASC'>Date ASC</option>
-								<option value='date DESC'>Date DESC</option>
-							</optgroup>
-						</Select>
-					</FormControl>
-				</div>
-			</div>
-
-			{open && (
-				<Modal
-					handleClose={handleClose}
-					applyFilters={handleApplyFilters}
-					searchTerm={searchTerm}
-					setSearchTerm={setSearchTerm}
-					instructions={appliedFilters.instructions}
-					sortOption={sortOption}
-					setSortOption={setSortOption}
-				/>
-			)}
-			<div>
-				<h2>Applied Filters:</h2>
-				{Array.isArray(selectedFilters.category) &&
-					selectedFilters.category.map((filter, index) => (
-						<Chip
-							key={index}
-							label={filter}
-							onDelete={() => handleDelete("category", filter)}
-						/>
-					))}
-				{Array.isArray(selectedFilters.tags) &&
-					selectedFilters.tags.map((filter, index) => (
-						<Chip
-							key={index}
-							label={filter}
-							onDelete={() => handleDelete("tags", filter)}
-						/>
-					))}
-				{Array.isArray(selectedFilters.ingredients) &&
-					selectedFilters.ingredients.map((filter, index) => (
-						<Chip
-							key={index}
-							label={filter}
-							onDelete={() => handleDelete("ingredients", filter)}
-						/>
-					))}
-				{selectedFilters.instructions !== null && (
-					<Chip
-						label={selectedFilters.instructions}
-						onDelete={() =>
-							handleDelete("instructions", selectedFilters.instructions)
-						}
-					/>
-				)}
-			</div>
-			{noFiltersApplied && <p>No filters have been applied.</p>}
-			<Chip
-				color="secondary"
-				label="Clear All Filters"
-				size="small"
-				variant="outlined"
-				onClick={handleResetFilters}
-			/>
-		</div>
-	);
+      {open && (
+        <Modal
+          handleClose={handleClose}
+          applyFilters={handleApplyFilters}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          instructions={appliedFilters.instructions}
+          sortOption={sortOption}
+          setSortOption={setSortOption}
+        />
+      )}
+      <div>
+        <h2>Applied Filters:</h2>
+        {Array.isArray(selectedFilters.category) &&
+          selectedFilters.category.map((filter, index) => (
+            <Chip
+              key={index}
+              label={filter}
+              onDelete={() => handleDelete("category", filter)}
+            />
+          ))}
+        {Array.isArray(selectedFilters.tags) &&
+          selectedFilters.tags.map((filter, index) => (
+            <Chip
+              key={index}
+              label={filter}
+              onDelete={() => handleDelete("tags", filter)}
+            />
+          ))}
+        {Array.isArray(selectedFilters.ingredients) &&
+          selectedFilters.ingredients.map((filter, index) => (
+            <Chip
+              key={index}
+              label={filter}
+              onDelete={() => handleDelete("ingredients", filter)}
+            />
+          ))}
+        {selectedFilters.instructions !== null && (
+          <Chip
+            label={selectedFilters.instructions}
+            onDelete={() =>
+              handleDelete("instructions", selectedFilters.instructions)
+            }
+          />
+        )}
+      </div>
+      {noFiltersApplied && <p>No filters have been applied.</p>}
+      <Chip
+        color="secondary"
+        label="Clear All Filters"
+        size="small"
+        variant="outlined"
+        onClick={handleResetFilters}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
