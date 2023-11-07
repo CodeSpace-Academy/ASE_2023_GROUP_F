7e35d5ba4a727654a31a146400bf57f9a434325a
chemonique <122254404+chemonique@users.noreply.github.com>
2023-11-07 08:37:50 +0200

    replace RecipeSkeleton w Animation

--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -1,47 +1,46 @@
-import Details from "@/components/details/details";
-import { getSingleRecipe } from "@/lib/view-recipes";
-import { useState, useEffect, useContext } from "react";
-import RecipeSkeleton from "@/components/skeletonCard/detailPageSkeleton";
-import { filterContext } from '../../components/search-functionality/filterContext';
+import Details from '@/components/details/details'
+import { getSingleRecipe } from '@/lib/view-recipes'
+import { useState, useEffect, useContext } from 'react'
+import RecipeSkeleton from '@/components/skeletonCard/detailPageSkeleton'
+import { filterContext } from '../../components/search-functionality/filterContext'
+import Animation from '@/components/skeletonCard/loadingAnimation/LoadingAnimation'
 
 function SingleRecipe({ recipeId }) {
   const { filters, setFiltes } = useContext(filterContext)
-  const [recipe, setRecipe] = useState(null);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [recipe, setRecipe] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     console.log('filters in recipeId', filters)
     async function getRecipeById() {
       try {
-        const result = await getSingleRecipe(recipeId);
-        setRecipe(result.recipe);
-        setLoading(false);
+        const result = await getSingleRecipe(recipeId)
+        setRecipe(result.recipe)
+        setLoading(false)
       } catch (error) {
-        console.error(`something went wrong: ${error}`);
-        setError('Error fetching recipe data.');
-        setLoading(false); 
+        console.error(`something went wrong: ${error}`)
+        setError('Error fetching recipe data.')
+        setLoading(false)
       }
     }
 
-    getRecipeById();
-  }, [recipeId]);
+    getRecipeById()
+  }, [recipeId])
 
   return (
-    <>
-      {loading ? <RecipeSkeleton /> : <Details recipe={recipe} error={error} />}
-    </>
-  );
+    <>{loading ? <Animation /> : <Details recipe={recipe} error={error} />}</>
+  )
 }
 
-export default SingleRecipe;
+export default SingleRecipe
 
 export async function getServerSideProps({ params }) {
-  const { recipeId } = params;
+  const { recipeId } = params
 
   return {
     props: {
-      recipeId
-    }
-  };
+      recipeId,
+    },
+  }
 }
