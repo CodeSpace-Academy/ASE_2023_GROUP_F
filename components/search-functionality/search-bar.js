4b723b87bca33c48f860d504f0c5dc3e96230b0e
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-11-09 13:33:01 +0200

    Fixed auto-submit.

--- components/search-functionality/search-bar.js
+++ components/search-functionality/search-bar.js
@@ -5,122 +5,138 @@ import Modal from "./Modal";
 import { filterContext } from "./filterContext";
 
 const SearchBar = (props) => {
-	const {
-		applyFilters,
-		appliedFilters,
-		searchTerm,
-		setSearchTerm,
-		sortOption,
-		setSortOption,
-	} = props;
-	const [open, setOpen] = useState(false);
-	const [noFiltersApplied, setNoFiltersApplied] = useState(true);
-	const [updateAppliedFilter, setUpdateAppliedfilter] = useState({
-		category: [],
-		tags: [],
-		ingredients: [],
-		instructions: null,
-	});
+  const {
+    applyFilters,
+    appliedFilters,
+    searchTerm,
+    setSearchTerm,
+    sortOption,
+    setSortOption,
+  } = props;
+  const [open, setOpen] = useState(false);
+  const [noFiltersApplied, setNoFiltersApplied] = useState(true);
+  const [updateAppliedFilter, setUpdateAppliedfilter] = useState({
+    category: [],
+    tags: [],
+    ingredients: [],
+    instructions: null,
+  });
 
-	const [selectedFilters, setSelectedFilters] = useState({
-		category: [],
-		tags: [],
-		ingredients: [],
-		instructions: null,
-	});
+  const [selectedFilters, setSelectedFilters] = useState({
+    category: [],
+    tags: [],
+    ingredients: [],
+    instructions: null,
+  });
 
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
+  const [queryType, setQueryType] = useState("short");
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonEnabled, setButtonEnabled] = useState(true);
 
-	const handleApplyFilters = async (filters) => {
-		const nonEmptyFilters = {};
-		for (const key in filters) {
-			if (
-				filters[key] !== null &&
-				filters[key] !== "" &&
-				filters[key].length > 0
-			) {
-				nonEmptyFilters[key] = filters[key];
-			}
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-      setNoFiltersApplied(false)
+  const shortQueryDebounce = debounce((query) => {
+    applyFilters({ title: query }, sortOption);
+    setButtonEnabled(true);
+  }, 500);
+
+  const longQueryDebounce = debounce((query) => {
+    setButtonEnabled(true);
+  }, 1000);
+
+  const handleQueryChange = (query) => {
+    setSearchTerm(query);
+    setIsLoading(true);
+
+    if (query.length < 10) {
+      setQueryType("short");
+      shortQueryDebounce(query);
+    } else {
+      setQueryType("long");
+      longQueryDebounce(query);
     }
+  };
 
-		if (Object.keys(nonEmptyFilters).length > 0) {
-			await applyFilters(nonEmptyFilters, sortOption);
-		}
-		setSelectedFilters(filters);
-	};
-	const handleDelete = (filterType, filterValue) => {
-		const updatedFilters = { ...selectedFilters };
-		updatedFilters[filterType] = updatedFilters[filterType].filter(
-			(item) => item !== filterValue,
-		);
-		setSelectedFilters(updatedFilters);
-		setUpdateAppliedfilter(updatedFilters);
+  const handleDelete = (filterType, filterValue) => {
+    const updatedFilters = { ...selectedFilters };
+    updatedFilters[filterType] = updatedFilters[filterType].filter(
+      (item) => item !== filterValue
+    );
+    setSelectedFilters(updatedFilters);
+    setUpdateAppliedfilter(updatedFilters);
 
-		handleApplyFilters(updatedFilters);
-	};
+    applyFilters(updatedFilters);
+  };
 
-	const handleSort = async (event) => {
-		const newSortOption = event.target.value;
-		setSortOption(newSortOption);
-		await applyFilters(selectedFilters, newSortOption);
-	};
+  const handleSort = async (event) => {
+    const newSortOption = event.target.value;
+    setSortOption(newSortOption);
+    await applyFilters(selectedFilters, newSortOption);
+  };
 
-	const handleResetFilters = () => {
-		setSelectedFilters({
-			category: [],
-			tags: [],
-			ingredients: [],
-			instructions: null,
-		});
-		applyFilters({}, sortOption);
-		setNoFiltersApplied(true);
-	};
+  const handleResetFilters = () => {
+    setSelectedFilters({
+      category: [],
+      tags: [],
+      ingredients: [],
+      instructions: null,
+    });
+    applyFilters({}, sortOption);
+    setNoFiltersApplied(true);
+  };
 
-	useEffect(() => {
-		const debouncedApplyFilters = debounce((title) => {
-			applyFilters({ title }, sortOption);
-		}, 500);
+  const handleLongQuerySubmit = () => {
+    applyFilters({ title: searchTerm });
+  };
 
-		debouncedApplyFilters(searchTerm);
+  useEffect(() => {
+    if (queryType === "short") {
+      const delay = 1000; 
+      const timer = setTimeout(() => {
+        if (searchTerm.length < 10) {
+          handleLongQuerySubmit();
+        }
+      }, delay);
+      return () => clearTimeout(timer);
+    }
+  }, [searchTerm, queryType, handleLongQuerySubmit]);
 
-		return () => {
-			debouncedApplyFilters.cancel();
-		};
-	}, [searchTerm, sortOption]);
-
-	return (
-		<div>
-			<div className="flex container items-center justify-between">
-				<Button
-					variant="outlined"
-					size="large"
-					onClick={handleOpen}
-					className="border-gray-800 dark:text-blue-950 hover:text-white border hover:bg-gray-900 rounded-full"
-				>
-					Filters
-				</Button>
-				<div className="flex mx-auto gap-80 items-center space-x-5">
-					<label htmlFor="search" />
-					<input
-						className="rounded text-2xl p-2"
-						type="text"
-						id="search"
-						placeholder="Search...."
-						value={searchTerm}
-						onChange={(e) => setSearchTerm(e.target.value)}
-					/>
-
+  return (
+    <div>
+      <div className="flex container items-center justify-between">
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={handleOpen}
+          className="border-gray-800 dark:text-blue-950 hover:text-white border hover-bg-gray-900 rounded-full"
+        >
+          Filters
+        </Button>
+        <div className="flex mx-auto gap-10 items-center space-x-5">
+          <label htmlFor="search" />
+          <input
+            className="rounded text-2xl p-2"
+            type="text"
+            id="search"
+            placeholder="Search...."
+            value={searchTerm}
+            onChange={(e) => handleQueryChange(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLongQuerySubmit}
+          >
+            Submit
+          </Button>
           <FormControl
-            className="border-gray-800 hover:bg-slate-200"
+            className="border-gray-800 hover-bg-slate-200"
             sx={{ m: 1, minWidth: 120 }}
           >
             <InputLabel htmlFor="grouped-native-select">Sort By</InputLabel>
             <Select
-              native
-             
               id="grouped-native-select"
               label="Grouping"
               name="sortOption"
@@ -151,7 +167,7 @@ const SearchBar = (props) => {
 
       {open && (
         <Modal
-          handleClose={handleClose}
+          handleClose={() => setOpen(false)}
           applyFilters={handleApplyFilters}
           searchTerm={searchTerm}
           setSearchTerm={setSearchTerm}
@@ -181,7 +197,9 @@ const SearchBar = (props) => {
           <Chip
             key={selectedFilters.ingredients}
             label={selectedFilters.ingredients}
-            onDelete={() => handleDelete("ingredients", selectedFilters.ingredients)}
+            onDelete={() =>
+              handleDelete("ingredients", selectedFilters.ingredients)
+            }
           />
         )}
         {selectedFilters.instructions !== null && (
@@ -193,7 +211,11 @@ const SearchBar = (props) => {
           />
         )}
       </div>
-      {noFiltersApplied && <p className="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-2 my-4 rounded-md">No filters have been applied.</p>}
+      {noFiltersApplied && (
+        <p className="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-2 my-4 rounded-md">
+          No filters have been applied.
+        </p>
+      )}
       <Chip
         color="secondary"
         label="Clear All Filters"
