0321837bbc6635735b52cba0b212170720d4c887
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-11-13 13:15:03 +0200

    Corrected conflicts and added Multi select for ingredients

---
+++ components/search-functionality/ingredients-selection/multi-select-ingredients.js
@@ -0,0 +1,45 @@
+import { Autocomplete, TextField } from "@mui/material";
+import { getIngredients } from "@/lib/view-recipes";
+import { useEffect, useState } from "react";
+
+function MultiSelect({ingredients, setIngredients, ingredientsOptions, setIngredientsOptions}) {
+
+  async function gettingIngredients() {
+    try {
+      const result = await getIngredients();
+      setIngredients(result.uniqueIngredients[0].ingredientsArray);
+    } catch (error) {
+      console.log("Error fetching more recipes: ", error);
+    }
+  }
+
+  useEffect(() => {
+    try {
+      gettingIngredients();
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }, []);
+
+  return (
+    <Autocomplete
+      multiple
+      id="ingredients"
+      options={ingredients}
+      getOptionLabel={(option) => option}
+      value={ingredientsOptions}
+      onChange={(event, newValue) => {
+        if (newValue !== undefined && Array.isArray(newValue)) {
+          setIngredientsOptions(newValue);
+        } else {
+          setIngredientsOptions([]);
+        }
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Ingredients" />
+      )}
+    />
+  );
+}
+
+export default MultiSelect;
