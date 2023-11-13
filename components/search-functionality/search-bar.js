8762b733b46cc5d5c8c40ceceef6ee1c39c77068
Matere29 <makgakarorisang@gmail.com>
2023-11-13 08:55:21 +0200

    fixing search bar

--- components/search-functionality/search-bar.js
+++ components/search-functionality/search-bar.js
@@ -108,6 +108,23 @@ const SearchBar = ({
           <span class="hidden md:inline-block ml-2">Filters</span>
         </Button>
 
+        <div className="relative flex items-center">
+          <label htmlFor="search" className="sr-only">Search</label>
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-400">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </div>
+          <input
+            className="w-full md:min-w-[400px] p-2 pl-10 text-2xl rounded-full"
+            type="text"
+            id="search"
+            placeholder="Search...."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+
         <FormControl
           className="flex items-center border-gray-800 rounded-full hover:bg-slate-200"
           sx={{ m: 1, minWidth: 50 }}
@@ -147,18 +164,9 @@ const SearchBar = ({
             </optgroup>
           </Select>
         </FormControl>
-        
-        <div className="relative flex items-center">
-          <label htmlFor="search" className="sr-only">Search</label>
-          <input
-            className="w-full p-2 pl-10 text-2xl rounded-full"
-            type="text"
-            id="search"
-            placeholder="Search...."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
+
+       
+
 
 
       </div>
