446679ec8f6c8c12caa8ea8f9c5cd80fe72fc2a7
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-11-14 12:01:17 +0200

    Improving functions

--- components/search-functionality/search-bar.js
+++ components/search-functionality/search-bar.js
@@ -53,6 +53,7 @@ const SearchBar = ({
     }
     setSelectedFilters(filters);
   };
+
   const handleDelete = (filterType, filterValue) => {
     const updatedFilters = { ...selectedFilters };
     updatedFilters[filterType] = updatedFilters[filterType].filter(
@@ -81,8 +82,13 @@ const SearchBar = ({
     setNoFiltersApplied(true);
   };
 
-  const handleLongQuerySubmit = () => {
-    applyFilters({ title: searchTerm });
+  const handleQuerySubmit = () => {
+    if (searchTerm.length < 10) {
+      applyFilters({ title: searchTerm });
+    } else {
+      // handle long query
+      setButtonEnabled(true); // You might want to do something here for long queries
+    }
   };
 
   useEffect(() => {
@@ -94,6 +100,7 @@ const SearchBar = ({
     }, 500);
 
     const longQueryDebounce = debounce((query) => {
+      // handle long query
       setButtonEnabled(true);
     }, 1000);
 
@@ -134,73 +141,76 @@ const SearchBar = ({
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
           </svg>
-
           <span class="hidden md:inline-block ml-2">Filters</span>
         </Button>
         <div className="flex mx-auto gap-10 items-center space-x-5">
           <label htmlFor="search" />
-
-        <div className="relative flex items-center">
-          <label htmlFor="search" className="sr-only">Search</label>
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-400">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-          </div>
-          <input
-            className="w-full md:min-w-[400px] p-2 pl-10 text-2xl rounded-full"
-            type="text"
-            id="search"
-            placeholder="Search...."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
-
-        <FormControl
-          className="flex items-center border-gray-800 rounded-full hover:bg-slate-200"
-          sx={{ m: 1, minWidth: 50 }}
-        >
-          <InputLabel htmlFor="grouped-native-select" className="items-center rounded-full md:flex">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 md:mr-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-            </svg>
-            <span class="hidden md:inline-block">Sort By</span>
-          </InputLabel>
-
-          <Select
-            native
-            defaultValue=""
-            id="grouped-native-select"
-            label="Grouping"
-            name="sortOption"
-            value={sortOption}
-            onChange={handleSort}
+          <div className="relative flex items-center">
+            <label htmlFor="search" className="sr-only">Search</label>
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </div>
+            <input
+              className="w-full md:min-w-[400px] p-2 pl-10 text-2xl rounded-full"
+              type="text"
+              id="search"
+              placeholder="Search...."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+              <Button
+            variant="outlined"
+            size="large"
+            onClick={handleQuerySubmit}
+            className="flex items-center border border-gray-800 rounded-full dark:text-blue-950 hover:text-white hover:bg-gray-900"
+            disabled={!buttonEnabled}
           >
-            <option aria-label="None" value="" />
-            <optgroup name="prep" label="Prep Time">
-              <option value="prep ASC">Prep ASC</option>
-              <option value="prep DESC">Prep DESC</option>
-            </optgroup>
-            <optgroup name="cook" label="Cook Time">
-              <option value="cook ASC">Cook ASC</option>
-              <option value="cook DESC">Cook DESC</option>
-            </optgroup>
-            <optgroup name="published" label="Date Created">
-              <option value="date ASC">Date ASC</option>
-              <option value="date DESC">Date DESC</option>
-            </optgroup>
-            <optgroup name="instructions" label="Instructions">
-              <option value="instructions ASC">Instructions ASC</option>
-              <option value="instructions DESC">Instructions DESC</option>
-            </optgroup>
-          </Select>
-        </FormControl>
+            Submit
+          </Button>
+          </div>
 
-       
-
-
-
+          <FormControl
+            className="flex items-center border-gray-800 rounded-full hover:bg-slate-200"
+            sx={{ m: 1, minWidth: 50 }}
+          >
+            <InputLabel htmlFor="grouped-native-select" className="items-center rounded-full md:flex">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 md:mr-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+              </svg>
+              <span class="hidden md:inline-block">Sort By</span>
+            </InputLabel>
+            <Select
+              native
+              defaultValue=""
+              id="grouped-native-select"
+              label="Grouping"
+              name="sortOption"
+              value={sortOption}
+              onChange={handleSort}
+            >
+              <option aria-label="None" value="" />
+              <optgroup name="prep" label="Prep Time">
+                <option value="prep ASC">Prep ASC</option>
+                <option value="prep DESC">Prep DESC</option>
+              </optgroup>
+              <optgroup name="cook" label="Cook Time">
+                <option value="cook ASC">Cook ASC</option>
+                <option value="cook DESC">Cook DESC</option>
+              </optgroup>
+              <optgroup name="published" label="Date Created">
+                <option value="date ASC">Date ASC</option>
+                <option value="date DESC">Date DESC</option>
+              </optgroup>
+              <optgroup name="instructions" label="Instructions">
+                <option value="instructions ASC">Instructions ASC</option>
+                <option value="instructions DESC">Instructions DESC</option>
+              </optgroup>
+            </Select>
+          </FormControl>
+        
+        </div>
       </div>
 
       {open && (
