241a79a97ab3a2176cba640553322bb2065d08f4
NelsonMALGAS <zmalgas69@gmail.com>
2023-11-27 19:49:59 +0200

    Fix skeleton display

--- components/skeletonCard/skeleton.js
+++ components/skeletonCard/skeleton.js
@@ -5,7 +5,7 @@ const CardSkeleton = () => {
   return (
     <div style={{ width: '100%', height: '500px', display: 'flex', flexWrap: 'wrap' }}>
       {Array.from({ length: 20 }, (_, index) => (
-        <div key={index} style={{ width: '20%', padding: '20px' }}>
+        <div key={index} style={{ width: '25%', padding: '20px' }}>
           <Skeleton variant="rectangular" width="100%" height={300} style={{ borderRadius:'5px'}} />
           <Skeleton variant="rectangular" width='100%' height={40} style={{ marginTop: '10px' }} />
           <Skeleton variant="rectangular" width='100%' height={40} style={{ marginTop: '10px' }} />
