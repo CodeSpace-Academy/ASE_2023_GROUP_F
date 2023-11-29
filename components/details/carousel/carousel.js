ac90eaed814e7791a01b892d3637c8898d3e3999
chemonique <122254404+chemonique@users.noreply.github.com>
2023-11-29 16:01:19 +0200

    view the image in different views

--- components/details/carousel/carousel.js
+++ components/details/carousel/carousel.js
@@ -1,5 +1,5 @@
-import { Button } from "@mui/material"
-import Image from "next/image"
+import { Button } from "@mui/material";
+import Image from "next/image";
 
 /**
  * Carousel Component
@@ -16,17 +16,22 @@ import Image from "next/image"
  *
  * */
 
-function Carousel({imageList, setFunc}){
-
-  return(
-    <div className="mr-5 bg-gray-200 p-5 flex flex-col justify-between overflow-y-auto max-h-96">
+function Carousel({ imageList, setFunc }) {
+  return (
+    <div className="mr-5 bg-gray-200 p-5 flex flex-row justify-between overflow-y-auto max-h-96">
       {imageList.map((image, index) => (
         <Button onClick={() => setFunc(index)} key={image}>
-          <Image src={image} alt="recipe image" width={200} height={200} className="mb-4"/>
+          <Image
+            src={image}
+            alt="recipe image"
+            width={200}
+            height={200}
+            className="mb-4"
+          />
         </Button>
       ))}
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- components/details/recipeBanner/recipeBanner.js
+++ components/details/recipeBanner/recipeBanner.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import { Button } from '@mui/material'
-import { ArrowCircleLeft, ArrowCircleRight } from '@mui/icons-material'
-import Link from 'next/link'
-import { useState } from 'react'
-import Carousel from '../carousel/carousel'
+import Image from "next/image";
+import { Button } from "@mui/material";
+import { ArrowCircleLeft, ArrowCircleRight } from "@mui/icons-material";
+import Link from "next/link";
+import { useState } from "react";
+import Carousel from "../carousel/carousel";
 
 /**
  * RecipeBanner Component
@@ -13,20 +13,19 @@ import Carousel from '../carousel/carousel'
  */
 
 function RecipeBanner({ images }) {
-
   // State variable to track the index of the currently displayed image
-  const [currentImage, setCurrentImage] = useState(0)
+  const [currentImage, setCurrentImage] = useState(0);
 
   function next() {
-    const isFirstImage = currentImage === images.length - 1
-    const newImage = isFirstImage ? 0 : currentImage + 1
-    setCurrentImage(newImage)
+    const isFirstImage = currentImage === images.length - 1;
+    const newImage = isFirstImage ? 0 : currentImage + 1;
+    setCurrentImage(newImage);
   }
 
   function prev() {
-    const isFirstImage = currentImage === 0
-    const newImage = isFirstImage ? images.length - 1 : currentImage - 1
-    setCurrentImage(newImage)
+    const isFirstImage = currentImage === 0;
+    const newImage = isFirstImage ? images.length - 1 : currentImage - 1;
+    setCurrentImage(newImage);
   }
 
   /**
@@ -34,14 +33,13 @@ function RecipeBanner({ images }) {
    * @param {number} param - The index of the image to be displayed.
    */
   function imageHandler(param) {
-    setCurrentImage(param)
+    setCurrentImage(param);
   }
 
-  const areButtonsVisible = images.length > 1
+  const areButtonsVisible = images.length > 1;
 
   return (
-    <div className="relative flex flex-row">
-      <Carousel imageList={images} setFunc={imageHandler} />
+    <div className="relative flex flex-col">
       <div>
         <Image
           src={images[currentImage]}
@@ -49,13 +47,13 @@ function RecipeBanner({ images }) {
           width={650}
           height={650}
         />
-        <div className="absolute top-0 right-0">
+        {/* <div className="absolute top-0 right-0">
           <Link href="/recipes/instructions">
             <Button variant="contained" color="secondary" size="small">
               Start Cooking
             </Button>
           </Link>
-        </div>
+        </div> */}
         {areButtonsVisible && (
           <>
             <div className="absolute top-32">
@@ -67,8 +65,9 @@ function RecipeBanner({ images }) {
           </>
         )}
       </div>
+      <Carousel imageList={images} setFunc={imageHandler} />
     </div>
-  )
+  );
 }
 
-export default RecipeBanner
+export default RecipeBanner;
