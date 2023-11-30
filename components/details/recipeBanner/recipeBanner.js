45edc3147c681fe89afccf93e7683359e1a604f4
chemonique <122254404+chemonique@users.noreply.github.com>
2023-11-30 16:59:57 +0200

    image sized changes

--- components/details/recipeBanner/recipeBanner.js
+++ components/details/recipeBanner/recipeBanner.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import { Button } from '@mui/material'
-import { ArrowCircleLeft, ArrowCircleRight } from '@mui/icons-material'
-import Link from 'next/link'
-import { useState } from 'react'
-import Carousel from '../carousel/carousel'
+import Image from "next/image";
+import { Button } from "@mui/material";
+import { ArrowCircleLeft, ArrowCircleRight } from "@mui/icons-material";
+import Link from "next/link";
+import { useState } from "react";
+import Carousel from "../carousel/carousel";
 
 /**
  * RecipeBanner Component
@@ -13,20 +13,19 @@ import Carousel from '../carousel/carousel'
  */
 
 function RecipeBanner({ images }) {
-
   // State variable to track the index of the currently displayed image
-  const [currentImage, setCurrentImage] = useState(0)
+  const [currentImage, setCurrentImage] = useState(0);
 
   function next() {
-    const isFirstImage = currentImage === images.length - 1
-    const newImage = isFirstImage ? 0 : currentImage + 1
-    setCurrentImage(newImage)
+    const isFirstImage = currentImage === images.length - 1;
+    const newImage = isFirstImage ? 0 : currentImage + 1;
+    setCurrentImage(newImage);
   }
 
   function prev() {
-    const isFirstImage = currentImage === 0
-    const newImage = isFirstImage ? images.length - 1 : currentImage - 1
-    setCurrentImage(newImage)
+    const isFirstImage = currentImage === 0;
+    const newImage = isFirstImage ? images.length - 1 : currentImage - 1;
+    setCurrentImage(newImage);
   }
 
   /**
@@ -34,41 +33,35 @@ function RecipeBanner({ images }) {
    * @param {number} param - The index of the image to be displayed.
    */
   function imageHandler(param) {
-    setCurrentImage(param)
+    setCurrentImage(param);
   }
 
-  const areButtonsVisible = images.length > 1
+  const areButtonsVisible = images.length > 1;
 
   return (
-    <div className="relative flex flex-row">
-      <Carousel imageList={images} setFunc={imageHandler} />
-      <div>
+    <div className="relative">
+      <div style={{ height: "40vh", overflow: "hidden", position: "relative" }}>
         <Image
           src={images[currentImage]}
           alt="recipe image"
-          width={650}
-          height={650}
+          layout="fill"
+          objectFit="cover"
         />
-        <div className="absolute top-0 right-0">
-          <Link href="/recipes/instructions">
-            <Button variant="contained" color="secondary" size="small">
-              Start Cooking
-            </Button>
-          </Link>
-        </div>
-        {areButtonsVisible && (
-          <>
-            <div className="absolute top-32">
-              <Button startIcon={<ArrowCircleLeft />} onClick={prev}></Button>
-            </div>
-            <div className="absolute top-32 right-0">
-              <Button endIcon={<ArrowCircleRight />} onClick={next}></Button>
-            </div>
-          </>
-        )}
       </div>
+      {areButtonsVisible && (
+        <>
+          <div className="absolute left-0 top-1/3 transform-translate-y-1/3 z-1">
+            <Button startIcon={<ArrowCircleLeft />} onClick={prev}></Button>
+          </div>
+          <div className="absolute right-0 top-1/3 transform-translate-y-1/3 z-1">
+            <Button endIcon={<ArrowCircleRight />} onClick={next}></Button>
+          </div>
+        </>
+      )}
+
+      <Carousel imageList={images} setFunc={imageHandler} />
     </div>
-  )
+  );
 }
 
-export default RecipeBanner
+export default RecipeBanner;
