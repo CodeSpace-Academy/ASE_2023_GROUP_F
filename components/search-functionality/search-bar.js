e0f7073a93990a0ede8c9f4c7e366fe9caab7036
NelsonMALGAS <zmalgas69@gmail.com>
2023-11-30 10:03:18 +0200

    wip

--- components/search-functionality/search-bar.js
+++ components/search-functionality/search-bar.js
@@ -69,65 +69,69 @@ const SearchBar = (props) => {
 		await applyFilters(filters);
 	};
 
+
 	const handleDelete = async (filterType, filterValue) => {
-		setFilters(async (prevFilters) => {
-			const updatedFilters = { ...prevFilters };
-
-			if (Array.isArray(updatedFilters[filterType])) {
-				updatedFilters[filterType] = updatedFilters[filterType].filter(
-					(item) => item !== filterValue,
-				);
-
-				if (updatedFilters[filterType]?.length === 0) {
-					updatedFilters[filterType] = [];
-				}
-			} else {
-				if (filterType === "category" || filterType === "ingredients") {
-					updatedFilters[filterType] = null;
-				} else if (filterType === "instructions") {
-					updatedFilters[filterType] = null;
-				}
-			}
-
-			setUpdateAppliedfilter(updatedFilters);
-			await applyFilters(updatedFilters);
-
-			setSelectedFilters((prevFilters) => {
-				const updatedSelectedFilters = { ...prevFilters };
-
-				if (Array.isArray(updatedSelectedFilters[filterType])) {
-					updatedSelectedFilters[filterType] = updatedSelectedFilters[
-						filterType
-					].filter((item) => item !== filterValue);
-
-					if (updatedSelectedFilters[filterType]?.length === 0) {
-						updatedSelectedFilters[filterType] = {};
-					}
-				} else {
-					if (filterType === "category" || filterType === "ingredients") {
-						updatedSelectedFilters[filterType] = null;
-					} else if (filterType === "instructions") {
-						updatedSelectedFilters[filterType] = null;
-					}
-				}
-
-				return updatedSelectedFilters;
-			});
-
-			const hasNoFiltersLeft = Object.values(updatedFilters).every(
-				(value) =>
-					value === null ||
-					(Array.isArray(value) && value?.length === 0) ||
-					value === "",
+		setFilters((prevFilters) => {
+		  const updatedFilters = { ...prevFilters };
+	  
+		  if (Array.isArray(updatedFilters[filterType])) {
+			updatedFilters[filterType] = updatedFilters[filterType].filter(
+			  (item) => item !== filterValue
 			);
-
-			if (hasNoFiltersLeft) {
-				setNoFiltersApplied(true);
+	  
+			if (updatedFilters[filterType]?.length === 0) {
+			  updatedFilters[filterType] = [];
 			}
-
-			return updatedFilters;
+		  } else {
+			if (filterType === "category" || filterType === "ingredients") {
+			  updatedFilters[filterType] = null;
+			} else if (filterType === "instructions") {
+			  updatedFilters[filterType] = null;
+			}
+		  }
+	  
+		  setUpdateAppliedfilter(updatedFilters);
+		   applyFilters(updatedFilters);
+	  
+		  setSelectedFilters((prevFilters) => {
+			const updatedSelectedFilters = { ...prevFilters };
+	  
+			if (Array.isArray(updatedSelectedFilters[filterType])) {
+			  updatedSelectedFilters[filterType] = updatedSelectedFilters[
+				filterType
+			  ].filter((item) => item !== filterValue);
+	  
+			  if (updatedSelectedFilters[filterType]?.length === 0) {
+				updatedSelectedFilters[filterType] = null;
+			  }
+			} else {
+			  if (filterType === "category" || filterType === "ingredients") {
+				updatedSelectedFilters[filterType] = null;
+			  } else if (filterType === "instructions") {
+				updatedSelectedFilters[filterType] = null;
+			  }
+			}
+	  
+			return updatedSelectedFilters;
+		  });
+	  
+		  const hasNoFiltersLeft = Object.values(updatedFilters).every(
+			(value) =>
+			  value === null ||
+			  (Array.isArray(value) && value?.length === 0) ||
+			  value === ""
+		  );
+	  
+		  if (hasNoFiltersLeft) {
+			setNoFiltersApplied(true);
+		  } else {
+			setNoFiltersApplied(false);
+		  }
+	  
+		  return updatedFilters;
 		});
-	};
+	  };
+	  
 
 	const handleSort = async (event) => {
 		const newSortOption = event.target.value;
