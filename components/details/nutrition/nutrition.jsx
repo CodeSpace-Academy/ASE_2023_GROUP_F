bcf3e4bf343608a8d10e3df78c28134fae72d66e
Matere29 <makgakarorisang@gmail.com>
2023-12-06 15:32:10 +0200

    Fixing nutrition component

--- components/details/nutrition/nutrition.jsx
+++ components/details/nutrition/nutrition.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { Card, Button } from '@mui/material';
+import { Card } from '@mui/material';
 
 /**
- * Nutrition Component
- *
- * @param {Object} props - Component properties
- * @param {Object} props.nutritionList - An object representing the nutrition information.
- *
- * @returns {JSX.Element} Nutrition component
- */
+ 
+Nutrition Component*
+@param {Object} props - Component properties
+@param {Object} props.nutritionList - An object representing the nutrition information.
+*
+@returns {JSX.Element} Nutrition component
+*/
 
 function Nutrition({ nutritionList }) {
   const [showAll, setShowAll] = useState(false);
@@ -19,22 +19,33 @@ function Nutrition({ nutritionList }) {
   const visibleNutrition = showAll ? nutrition : nutrition.slice(0, 3);
 
   return (
-    <div className="flex-row mx-10 overflow-y-auto lg:rounded-xl md:rounded-sm lg:flex">
-      {visibleNutrition.map(([key, value]) => (
-        <Card sx={{ margin: 0.5, padding: 1 }} key={key}>
-          <div>
-            {key}: {value}
+    <>
+      <div className="flex-row mx-2 overflow-y-auto fontSize:5 lg:rounded-xl md:rounded-sm lg:flex xs:hidden">
+        {nutrition.map(([key, value]) => (
+          <Card sx={{ margin: 0.5, padding: 1 }} key={key}>
+            <div>
+              {key}: {value}
+            </div>
+          </Card>
+        ))}
+      </div>
+      <div className="flex-row mx-5 overflow-y-auto md:rounded-sm lg:flex ">
+        {visibleNutrition.map(([key, value]) => (
+          <Card sx={{ margin: 0.5, padding: 1 }} key={key}>
+            <div>
+              {key}: {value}
+            </div>
+          </Card>
+        ))}
+        {nutrition.length > 3 && (
+          <div className="flex flex-col items-center ">
+            <div onClick={() => setShowAll(!showAll)} sx={{ mt: 1 }}>
+              {showAll ? '▲' : ' ▼'}
+            </div>
           </div>
-        </Card>
-      ))}
-      {nutrition.length > 3 && (
-        <div className="flex flex-col items-center lg:hidden">
-          <Button onClick={() => setShowAll(!showAll)} sx={{ mt: 1 }}>
-            {showAll ? '▲' : ' ▼'}
-          </Button>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
